baae7cbba2b8f3813aadb3f88268485eb4f58ce3
leowu4ever <wupopi@gmail.com>
2015-10-27 17:27:22 +0000

    Update Shop Scene, Setting Scene and link four scenes together
    Add shop scene and setting scene, link the main scene with shop scene,
    setting scene and level map scene.

---
+++ Assets/Scripts/ButtonOperator.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ButtonOperator : MonoBehaviour
+{
+	public void LoadLvMapScene ()
+	{
+		Application.LoadLevel (1);
+	}
+
+	public void LoadShopScene ()
+	{
+		Application.LoadLevel (2);
+	}
+
+	public void LoadSettingScene ()
+	{
+		Application.LoadLevel (3);
+	}
+
+}
