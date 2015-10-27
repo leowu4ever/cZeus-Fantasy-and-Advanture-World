1ab63dc5575fc6d63280cad83a2ce3f547a71c78
yifanzhai <yifanzhai520@gmail.com>
2015-10-27 23:16:04 +0000

    Update Shop Scene
    Add back button and add back to main scene function

--- Assets/Scripts/ButtonOperator.cs
+++ Assets/Scripts/ButtonOperator.cs
@@ -7,15 +7,27 @@ public class ButtonOperator : MonoBehaviour
 	{
 		Application.LoadLevel (1);
 	}
-
-	public void LoadShopScene ()
-	{
-		Application.LoadLevel (2);
-	}
-
-	public void LoadSettingScene ()
+    public void LoadSettingScene()
+    {
+        Application.LoadLevel(2);
+    }
+    public void LoadShopScene ()
 	{
 		Application.LoadLevel (3);
 	}
+
+	
+    public void LvMapSceneBackToMainScene()
+    {
+        Application.LoadLevel(0);
+    }
+    public void SettingSceneBackToMainScene()
+    {
+        Application.LoadLevel(0);
+    }
+    public void ShopSceneBackToMainScene()
+    {
+        Application.LoadLevel(0);
+    }
 
 }
