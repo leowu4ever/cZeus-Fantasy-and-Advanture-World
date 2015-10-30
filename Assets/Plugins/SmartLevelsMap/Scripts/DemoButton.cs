faf2e64520594cfb15005a1748bebaa80ac67ff4
leowu4ever <wupopi@gmail.com>
2015-10-30 22:01:46 +0000

    I don't know what happened but it is working

--- Assets/Plugins/SmartLevelsMap/Scripts/DemoButton.cs
+++ Assets/Plugins/SmartLevelsMap/Scripts/DemoButton.cs
@@ -3,16 +3,15 @@ using UnityEngine;
 
 namespace Assets.Plugins.SmartLevelsMap.Scripts
 {
-    public class DemoButton : MonoBehaviour 
-    {
+	public class DemoButton : MonoBehaviour
+	{
 
-        public event EventHandler Click;
+		public event EventHandler Click;
 
-        public void OnMouseUpAsButton()
-        {
-            if (Click != null)
-                Click(this, EventArgs.Empty);
-        }
-
-    }
+		public void OnMouseUpAsButton ()
+		{
+			if (Click != null)
+				Click (this, EventArgs.Empty);
+		}
+	}
 }
--- Assets/Scripts/ButtonOperator.cs
+++ Assets/Scripts/ButtonOperator.cs
@@ -7,39 +7,38 @@ public class ButtonOperator : MonoBehaviour
 	{
 		Application.LoadLevel (1);
 	}
-    public void LoadSettingScene()
-    {
-        Application.LoadLevel(2);
-    }
-    public void LoadTutorialScene ()
+	public void LoadSettingScene ()
+	{
+		Application.LoadLevel (2);
+	}
+	public void LoadTutorialScene ()
 	{
 		Application.LoadLevel (3);
 	}
-
 	
-    public void LvMapSceneBackToMainScene()
-    {
-        Application.LoadLevel(0);
-    }
-    public void SettingSceneBackToMainScene()
-    {
-        Application.LoadLevel(0);
-    }
-    public void ShopSceneBackToLvMapScene()
-    {
-        Application.LoadLevel(1);
-    }
-    public void InGameSceneBackToLvMapScene()
-    {
-        Application.LoadLevel(1);
-    }
-    public void TutorialSceneBackToMainScene()
-    {
-        Application.LoadLevel(0);
-    }
-    public void LvMapSceneToShopScene()
-    {
-        Application.LoadLevel(5);
-    }
+	public void LvMapSceneBackToMainScene ()
+	{
+		Application.LoadLevel (0);
+	}
+	public void SettingSceneBackToMainScene ()
+	{
+		Application.LoadLevel (0);
+	}
+	public void ShopSceneBackToLvMapScene ()
+	{
+		Application.LoadLevel (1);
+	}
+	public void InGameSceneBackToLvMapScene ()
+	{
+		Application.LoadLevel (1);
+	}
+	public void TutorialSceneBackToMainScene ()
+	{
+		Application.LoadLevel (0);
+	}
+	public void LvMapSceneToShopScene ()
+	{
+		Application.LoadLevel (5);
+	}
 
 }
