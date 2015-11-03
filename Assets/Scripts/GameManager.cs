8ea997efc6aa94ee32ff391e9c1192631c6f3bae
leowu4ever <wupopi@gmail.com>
2015-11-03 17:09:12 +0000

    Add Game Manager

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour
+{
+
+	// Use this for initialization
+	void Start ()
+	{
+		if (GameObject.FindGameObjectsWithTag ("Board") != null) {
+			Debug.Log (GameObject.FindGameObjectsWithTag ("Board").Length);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	
+	}
+	void InitGameboard ()
+	{
+		
+
+	}
+}
