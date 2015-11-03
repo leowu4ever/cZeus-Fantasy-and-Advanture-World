de39d73a114f210ddbfb059f37a3965eafa05f43
leowu4ever <wupopi@gmail.com>
2015-11-03 17:33:12 +0000

    Add test part in Gamemanager script

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -3,13 +3,26 @@ using System.Collections;
 
 public class GameManager : MonoBehaviour
 {
-
+	private int boardCount;
 	// Use this for initialization
 	void Start ()
 	{
+		GameObject[] boardArray = GameObject.FindGameObjectsWithTag ("Board");					// find gameobject by tag 
+		int boardArraySize = boardArray.Length;
 		if (GameObject.FindGameObjectsWithTag ("Board") != null) {
-			Debug.Log (GameObject.FindGameObjectsWithTag ("Board").Length);
+			Debug.Log ("There are " + boardArraySize + " in the scene");
 		}
+
+		int tempCount = 0;
+		for (int i = 0; i < boardArraySize; i++) {
+			Debug.Log (boardArray [i].name);													// access gameobject name
+			tempCount++;
+		}
+
+		if (tempCount == boardArraySize) {
+			Debug.Log ("Correct!!!");
+		}
+	
 	}
 	
 	// Update is called once per frame
