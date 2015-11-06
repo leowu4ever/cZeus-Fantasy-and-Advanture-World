d9d7fd535e4b204fc8764a37aed285bf31095897
leowu4ever <wupopi@gmail.com>
2015-11-06 16:59:52 +0000

    Update content script and game manager script
    Now able to change content sprite from gamemanger

---
+++ Assets/Scripts/ContentScript.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System;
+
+using System.Collections;
+
+public class ContentScript : MonoBehaviour
+{
+	public Sprite[] contentSprites;
+	public string content = "0";
+	public bool isAnswered = false;
+	
+	void Start ()
+	{
+		switch (content) {
+		case "0":
+			break;
+		case "1":
+			break;
+		case "2":
+			break;
+		case "3":
+			break;
+		case "4":
+			break;
+		case "5": 
+			break;
+		case "6":
+			break;
+		case "7":
+			break;
+		case "8":
+			break;
+		case "9":
+			break;
+		default :
+			Debug.Log ("?");
+			break;
+		}
+		gameObject.GetComponent<SpriteRenderer> ().sprite = contentSprites [0];
+	}
+
+	void InitContentSprite ()
+	{
+
+	}
+	void Update ()
+	{
+	
+	}
+}
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -8,29 +8,8 @@ public class GameManager : MonoBehaviour
 	// Use this for initialization
 	void Start ()
 	{
-//		GameObject[] boardArray = GameObject.FindGameObjectsWithTag ("Board");					// find gameobject by tag 
-//		int boardArraySize = boardArray.Length;
-//		Debug.Log ("There are " + boardArraySize + " boards in the scene");
-//
-//
-//		int tempCount = 0;
-//		for (int i = 0; i < boardArraySize; i++) {
-//			Debug.Log (boardArray [i].name);													// access gameobject name
-//			tempCount++;
-//		}
-//
-//		if (tempCount == boardArraySize) {
-//			Debug.Log ("Correct!!!");
-//		}
-//
-//		// Generate a random gameboard
-//		GameObject[] boardContentArray = GameObject.FindGameObjectsWithTag ("Board Content");
-//		int boardContentArraySize = boardContentArray.Length;
-//		Debug.Log ("There are " + boardContentArraySize + " texts in the scene");
-//		for (int i = 0; i < boardContentArraySize; i++) {
-//			Debug.Log (boardContentArray [i].name);													// access gameobject name
-//			tempCount++;
-//		}
+		contentSpriteArray [0].GetComponent<ContentScript> ().content = "4";
+			
 	}
 	
 	void Update ()
