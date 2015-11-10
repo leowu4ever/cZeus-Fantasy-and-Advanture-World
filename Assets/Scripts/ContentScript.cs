771d3257784bd76cdaea604b7ed15a9d176dafa8
leowu4ever <wupopi@gmail.com>
2015-11-10 15:19:52 +0000

    Update CSV parser
    Able to hide numbers

--- Assets/Scripts/ContentScript.cs
+++ Assets/Scripts/ContentScript.cs
@@ -12,22 +12,22 @@ public class ContentScript : MonoBehaviour
 	void Start ()
 	{	
 		char[] contentCharArray = content.ToCharArray ();
-
-		if (contentCharArray.Length == 1) {
-			GameObject contentSprite = Instantiate (numberPrefab, gameObject.transform.position, Quaternion.identity) as GameObject;
-			contentSprite.transform.parent = gameObject.transform;
-			contentSprite.GetComponent<NumberSpriteScript> ().SetSpriteTo (int.Parse (contentCharArray [0].ToString ()));
-		}
-
-		if (contentCharArray.Length > 1) {
-			for (int a = 0; a < contentCharArray.Length; a++) {
-				GameObject contentSprite = Instantiate (numberPrefab, new Vector3 (transform.position.x + a * 0.15f, transform.position.y, transform.position.z), Quaternion.identity) as GameObject;
+		if (contentCharArray [0] != '0') {
+			if (contentCharArray.Length == 1) {
+				GameObject contentSprite = Instantiate (numberPrefab, gameObject.transform.position, Quaternion.identity) as GameObject;
 				contentSprite.transform.parent = gameObject.transform;
-				contentSprite.GetComponent<NumberSpriteScript> ().SetSpriteTo (int.Parse (contentCharArray [a].ToString ()));
+				contentSprite.GetComponent<NumberSpriteScript> ().SetSpriteTo (int.Parse (contentCharArray [0].ToString ()));
 			}
+
+			if (contentCharArray.Length > 1) {
+				for (int a = 0; a < contentCharArray.Length; a++) {
+					GameObject contentSprite = Instantiate (numberPrefab, new Vector3 (transform.position.x + a * 0.15f, transform.position.y, transform.position.z), Quaternion.identity) as GameObject;
+					contentSprite.transform.parent = gameObject.transform;
+					contentSprite.GetComponent<NumberSpriteScript> ().SetSpriteTo (int.Parse (contentCharArray [a].ToString ()));
+				}
+			}
+
 		}
-
-
 	}
 
 }
