252a29da3a9af23e35fdade71212d8883eaa7859
phuminp <phuminp@gmail.com>
2015-11-18 15:34:18 +0000

    Creating number sprite on gameboard

--- Assets/Scripts/InputNumberHandler.cs
+++ Assets/Scripts/InputNumberHandler.cs
@@ -13,11 +13,13 @@ public class InputNumberHandler : MonoBehaviour {
 
             if (BoardPressedHandler.currentPressedContent.tag == "Mystery Number Content")
             {
-                // assign new content to mystery number 
-                BoardPressedHandler.currentPressedContent.GetComponent<ContentScript>().content = buttonLabel;
-                GameObject contentSprite = Instantiate(numberPrefab, BoardPressedHandler.currentPressedContent.transform.position, Quaternion.identity) as GameObject;
-                contentSprite.GetComponent<NumberSpriteScript>().SetSpriteTo(int.Parse(BoardPressedHandler.currentPressedContent.GetComponent<ContentScript>().content));
-
+                if (buttonLabel == BoardPressedHandler.currentPressedContent.GetComponent<ContentScript>().answer)
+                {
+                    // assign new content to mystery number 
+                    BoardPressedHandler.currentPressedContent.GetComponent<ContentScript>().content = buttonLabel;
+                    GameObject contentSprite = Instantiate(numberPrefab, BoardPressedHandler.currentPressedContent.transform.position, Quaternion.identity) as GameObject;
+                    contentSprite.GetComponent<NumberSpriteScript>().SetSpriteTo(int.Parse(BoardPressedHandler.currentPressedContent.GetComponent<ContentScript>().content));
+                }
             }
         }
 
