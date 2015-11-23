9fdc576c49d97fef291de8536e1e85b138c1f5df
yifan.zhai <yifanzhai520@gmail>
2015-11-23 23:30:03 +0000

    Update MusicHandler Script
    Add SettingButtonPressed function and TutorialButtonPressed function

--- Assets/Scripts/MusicHandler.cs
+++ Assets/Scripts/MusicHandler.cs
@@ -4,11 +4,23 @@ using System.Collections;
 public class MusicHandler : MonoBehaviour {
 
 	public GameObject PlayButtonMusic;
+	public GameObject SettingButtonMusic;
+	public GameObject TutorialButtonMusic;
 
 	public void PlayButtonPressed(){
 
 		PlayButtonMusic.GetComponent<AudioSource> ().Play ();
 		Debug.Log(PlayButtonMusic.GetComponent<AudioSource> ().isPlaying);
+	}
+	public void SettingButtonPressed(){
+		
+		SettingButtonMusic.GetComponent<AudioSource> ().Play ();
+		Debug.Log(SettingButtonMusic.GetComponent<AudioSource> ().isPlaying);
+	}
+	public void TutorialButtonPressed(){
+		
+		TutorialButtonMusic.GetComponent<AudioSource> ().Play ();
+		Debug.Log(TutorialButtonMusic.GetComponent<AudioSource> ().isPlaying);
 	}
 
 }
