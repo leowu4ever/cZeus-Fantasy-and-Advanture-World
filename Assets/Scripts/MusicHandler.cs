af8142abaa1238fe37eb34a8e4e5c535de10ec66
yifan.zhai <yifanzhai520@gmail>
2015-11-23 23:16:19 +0000

    Add MusicHandler Script

---
+++ Assets/Scripts/MusicHandler.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MusicHandler : MonoBehaviour {
+
+	public GameObject PlayButtonMusic;
+
+	public void PlayButtonPressed(){
+
+		PlayButtonMusic.GetComponent<AudioSource> ().Play ();
+	}
+
+}
