babbb2ab1c1cc3e498bace7cfa884e71bb0ea28f
yifan.zhai <yifanzhai520@gmail>
2015-11-24 14:14:03 +0000

    Update Level Map
    Add level on Chapter one map

--- Assets/Scripts/MusicHandler.cs
+++ Assets/Scripts/MusicHandler.cs
@@ -14,22 +14,22 @@ public class MusicHandler : MonoBehaviour {
 	public void PlayButtonPressed(){
 
 		PlayButtonMusic.GetComponent<AudioSource> ().Play ();
-		Debug.Log(PlayButtonMusic.GetComponent<AudioSource> ().isPlaying);
+		//Debug.Log(PlayButtonMusic.GetComponent<AudioSource> ().isPlaying);
 	}
 	public void SettingButtonPressed(){
 		
 		SettingButtonMusic.GetComponent<AudioSource> ().Play ();
-		Debug.Log(SettingButtonMusic.GetComponent<AudioSource> ().isPlaying);
+		//Debug.Log(SettingButtonMusic.GetComponent<AudioSource> ().isPlaying);
 	}
 	public void TutorialButtonPressed(){
 		
 		TutorialButtonMusic.GetComponent<AudioSource> ().Play ();
-		Debug.Log(TutorialButtonMusic.GetComponent<AudioSource> ().isPlaying);
+		//Debug.Log(TutorialButtonMusic.GetComponent<AudioSource> ().isPlaying);
 	}
 	public void BackButtonPressed(){
 		
 		BackButtonMusic.GetComponent<AudioSource> ().Play ();
-		Debug.Log(TutorialButtonMusic.GetComponent<AudioSource> ().isPlaying);
+		//Debug.Log(TutorialButtonMusic.GetComponent<AudioSource> ().isPlaying);
 	}
 
 }
