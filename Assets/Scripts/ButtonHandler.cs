750daa54b129d494ee7ad73fd4a054a3c4f1af62
chenxidi <375900030@qq.com>
2015-11-24 16:19:32 +0000

    tutorial bug fixed

--- Assets/Scripts/ButtonHandler.cs
+++ Assets/Scripts/ButtonHandler.cs
@@ -9,7 +9,7 @@ public class ButtonHandler : MonoBehaviour
     }
     public void LoadSettingScene ()
 	{
-		Application.LoadLevel (1);
+		Application.LoadLevel (3);
 	}
 	public void LoadTutorialScene ()
 	{
@@ -27,4 +27,28 @@ public class ButtonHandler : MonoBehaviour
     {
         Application.LoadLevel(5);
     }
+	public void LoadTutorialPage1Scene ()
+	{
+		Application.LoadLevel (7);
+	}
+	public void LoadTutorialPage2Scene ()
+	{
+		Application.LoadLevel (9);
+	}
+	public void LoadTutorialPage3Scene ()
+	{
+		Application.LoadLevel (8);
+	}
+	public void LoadTutorialPage4Scene ()
+	{
+		Application.LoadLevel (10);
+	}
+	public void LoadTutorialPage5Scene ()
+	{
+		Application.LoadLevel (11);
+	}
+	public void LoadTutorialPage6Scene ()
+	{
+		Application.LoadLevel (12);
+	}
 }
