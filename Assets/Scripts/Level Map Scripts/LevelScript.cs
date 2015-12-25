11e8b1052867bfe29017d5051a9a3044cdb2522c
leowu4ever <wupopi@gmail.com>
2015-12-25 14:06:45 +0000

    Added levelScript

---
+++ Assets/Scripts/Level Map Scripts/LevelScript.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelScript : MonoBehaviour {
+
+    public int levelID;
+    public bool isLocked;
+    public int score;
+    
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
