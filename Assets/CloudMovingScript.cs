4edda1822611850e0c1018b4471d45aa0ea38f5f
leowu4ever <wupopi@gmail.com>
2015-12-25 08:25:48 +0000

    Add cloud moving animation in world map

---
+++ Assets/CloudMovingScript.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CloudMovingScript : MonoBehaviour {
+
+    private Vector3 startPointV3;    
+    public GameObject endPoint;
+    public float speed;
+    
+    void Start() {
+        startPointV3 = transform.position;
+        
+    }
+	// Update is called once per frame
+	void Update () {
+	   if (transform.position == startPointV3) {
+           LeanTween.move (gameObject, endPoint.transform.position, 1/speed);
+       } 
+       
+       if (transform.position == endPoint.transform.position) {
+           LeanTween.move (gameObject, startPointV3, 1/speed);
+       }
+	}
+}
