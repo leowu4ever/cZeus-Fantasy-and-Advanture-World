c3f889993bc14c0fb6e228e95350dcb4b08ec6f1
leowu4ever <wupopi@gmail.com>
2015-12-25 21:25:49 +0000

    Remove smart level map asset

--- Assets/Plugins/SmartLevelsMap/Scripts/IMapProgressManager.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Assets.Plugins.SmartLevelsMap.Scripts
-{
-    public interface IMapProgressManager
-    {
-        int LoadLevelStarsCount(int level);
-        void SaveLevelStarsCount(int level, int starsCount);
-        void ClearLevelProgress(int level);
-    }
-}
