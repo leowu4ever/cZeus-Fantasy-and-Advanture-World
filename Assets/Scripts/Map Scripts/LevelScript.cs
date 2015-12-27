ca454ee12cdf2d153a344faeb668071374378c53
leowu4ever <wupopi@gmail.com>
2015-12-27 22:09:40 +0000

    Lock feature is done for chapter 1

--- Assets/Scripts/Map Scripts/LevelScript.cs
+++ Assets/Scripts/Map Scripts/LevelScript.cs
@@ -6,8 +6,16 @@ public class LevelScript : MonoBehaviour {
     public int levelSceneId;
     public bool isLocked;
     public int stars;
-    
+    public GameObject levelBase;
     public GameObject locker;
     public string[] dialogs;
-
+    
+    void Start () {
+        if (isLocked) {
+            locker.SetActive (true);
+        }
+         else {
+            levelBase.SetActive (true);
+         }
+    }
 }
