a44a04203826314d1be7b9d81508700a513f689e
leowu4ever <wupopi@gmail.com>
2015-12-27 22:57:11 +0000

    Lock feature done in world map

--- Assets/Scripts/Map Scripts/ChapterScript.cs
+++ Assets/Scripts/Map Scripts/ChapterScript.cs
@@ -6,5 +6,14 @@ public class ChapterScript : MonoBehaviour {
     public int chapterSceneId;
     public bool isLocked;
     
-
+    public GameObject unlockedChapterNode;
+    public GameObject lockedChapterNode;
+    
+    void Start () {
+        if (isLocked) {
+            lockedChapterNode.SetActive (true);
+        }   else {
+            unlockedChapterNode.SetActive (true);
+        }
+    }
 }
