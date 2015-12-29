69160029d773d46533a48502ba600989f9df717e
leowu4ever <wupopi@gmail.com>
2015-12-29 11:34:46 +0000

    Updated hero animation on level screen

--- Assets/Scripts/Map Scripts/LevelWindowScript.cs
+++ Assets/Scripts/Map Scripts/LevelWindowScript.cs
@@ -7,8 +7,8 @@ public class LevelWindowScript : MonoBehaviour {
     public GameObject bird;
     
 	void Start () {
-
-        hero.GetComponent<Animator>().CrossFade("talk", 0f);
+        transform.localScale = new Vector3 (0f, 0f, 0f);
+        hero.GetComponent<Animator>().CrossFade("Talk", 0f);
 	}
 	
 	void Update () {
