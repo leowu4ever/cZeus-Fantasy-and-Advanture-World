3a2c3cdeb065c65ccac182d0d09b1123529143bd
phuminp <phuminp@gmail.com>
2015-12-31 01:06:21 +0000

    make input number bar to display only provided choice for level 1-5

--- Assets/Scripts/In-Game Scripts/WindowHandler.cs
+++ Assets/Scripts/In-Game Scripts/WindowHandler.cs
@@ -5,8 +5,12 @@ public class WindowHandler : MonoBehaviour
 {
 
 	public GameObject inputNumberBar;
+    //for level 1-5
+    public GameObject[] MysterList;
+    public GameObject[] mysterAnswerChoice;
+    
 
-	void Update ()
+    void Update ()
 	{
 		if (GameManager.isInputing) {
 			ActivateInputNumberBar ();
@@ -20,10 +24,43 @@ public class WindowHandler : MonoBehaviour
 	void ActivateInputNumberBar ()
 	{
 		inputNumberBar.SetActive (true);
-
-	}
+        if (GameManager.puzzleLv=="1"|| GameManager.puzzleLv == "2"|| GameManager.puzzleLv == "3"|| GameManager.puzzleLv == "4"|| GameManager.puzzleLv == "5")
+        {
+            SelectedInputNumberToDisplay();
+        }
+    }
 	void DeactivateInputNumberBar ()
 	{
 		inputNumberBar.SetActive (false);
 	}
+    void SelectedInputNumberToDisplay()
+    {
+        if (BoardPressedHandler.curPressedContent.GetComponent<ContentScript>().tag == "Mystery Number Content")
+        {
+            for (int i = 1; i < inputNumberBar.transform.childCount; i++)
+            {
+                inputNumberBar.transform.GetChild(i).gameObject.SetActive(false);
+                for (int j = 0; j < MysterList.Length; j++)
+                {
+                    if (BoardPressedHandler.curPressedBoard == MysterList[j])
+                    {
+                        for (int n = (mysterAnswerChoice.Length / MysterList.Length) * j; n < (mysterAnswerChoice.Length / MysterList.Length) * j + mysterAnswerChoice.Length / MysterList.Length; n++)
+                        {
+                            if (i == int.Parse(mysterAnswerChoice[n].transform.GetChild(0).GetComponent<ContentScript>().content))
+                            {
+                                inputNumberBar.transform.GetChild(i).gameObject.SetActive(true);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        else
+        {
+            for (int i = 1; i < inputNumberBar.transform.childCount; i++)
+            {
+                inputNumberBar.transform.GetChild(i).gameObject.SetActive(true);
+            }
+        }
+    }
 }
