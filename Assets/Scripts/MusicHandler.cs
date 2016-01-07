4f4d889b6bf43297057a6b73b49e9da06baae224
chenxidi <375900030@qq.com>
2016-01-07 02:04:15 +0000

    Sound control and sound effect

--- Assets/Scripts/MusicHandler.cs
+++ Assets/Scripts/MusicHandler.cs
@@ -1,4 +1,4 @@
-﻿using UnityEngine;
+using UnityEngine;
 using System.Collections;
 
 public class MusicHandler : MonoBehaviour {
@@ -8,17 +8,30 @@ public class MusicHandler : MonoBehaviour {
 	public GameObject TutorialButtonMusic;
 	public GameObject BackButtonMusic;
 	public GameObject BgMusic;
+	public GameObject PlayButton;
+	public GameObject SoundOff;
 
 
 	public void stop()
 	{
-		BgMusic = GameObject.Find("BgMusic");
-		if(BgMusic.GetComponent<AudioSource>().isPlaying)
-		    BgMusic.GetComponent<AudioSource>().Stop();
-		else
-			BgMusic.GetComponent<AudioSource>().Play();
+		BgMusic = GameObject.Find("Bg Music");
+		SoundOff = GameObject.Find("Sound Off Button");
+		if (BgMusic.GetComponent<AudioSource> ().isPlaying)
+			BgMusic.GetComponent<AudioSource> ().Stop ();
+		else 
+			BgMusic.GetComponent<AudioSource> ().Play ();
+	
+		 
+	}
+
+	public void SoundEffect()
+	{
+		BgMusic = GameObject.Find("Bg Music");
+		if(BgMusic.GetComponent<AudioSource> ().isPlaying)
+		    this.GetComponent<AudioSource> ().Play ();
 
 	}
+
 
 	public void SettingButtonPressed(){
 		
