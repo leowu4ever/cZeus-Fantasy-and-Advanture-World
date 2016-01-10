a09fa53f7593245021edf81ad0c7b7609c7681de
phuminp <phuminp@gmail.com>
2016-01-10 18:43:58 +0000

    Add Math Icon Indication

---
+++ Assets/MathIconIndication.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class MathIconIndication : MonoBehaviour {
+
+    public Sprite[] mathSymbols;
+
+    void Update ()
+    {
+
+        if (BoardPressedHandler.curPressedContent == null)
+        {
+            gameObject.GetComponent<Image>().enabled = false;
+        }
+        else if (BoardPressedHandler.curPressedContent.name == "Mystery Number Content")
+        {
+            gameObject.GetComponent<Image>().enabled = false;
+        }
+        else if (BoardPressedHandler.curPressedContent.name == "Addition Content")
+        {
+            gameObject.GetComponent<Image>().enabled = true;
+
+            gameObject.GetComponent<Image>().sprite = mathSymbols[0];
+
+        }
+        else if (BoardPressedHandler.curPressedContent.name == "Product Content")
+        {
+            gameObject.GetComponent<Image>().enabled = true;
+
+            gameObject.GetComponent<Image>().sprite = mathSymbols[1];
+
+        }
+    }
+}
+    
